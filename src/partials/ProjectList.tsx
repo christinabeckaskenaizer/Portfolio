ad28e05975cb6dbc934589de5aef4c74f5e061d0
Christina <c.beckask@gmail.com>
2023-06-21 10:25:10 -0700

    project description update

--- src/partials/ProjectList.tsx
+++ src/partials/ProjectList.tsx
@@ -18,7 +18,7 @@ const ProjectList = () => (
       <Project
         name="Whimzee"
         description="Whimzee is an Etsy clone that empowers users to engage in a dynamic online marketplace.
-        Users can sign up, create their own shops, post listings(new and used), purchase items, and browse through a diverse range of products.
+        Users can sign up, create their own shops, post listings (new and used), purchase items, and browse through a diverse range of products.
         Whimzee offers a seamless and user-friendly experience, connecting buyers and sellers in a vibrant ecosystem of creativity and commerce."
         link="https://whimz.gitlab.io/module3-project-gamma/"
         img={{
@@ -36,9 +36,9 @@ const ProjectList = () => (
       />
       <Project
         name="CarCar"
-        description="CarCar is a powerful application designed to streamline the operations of car dealerships.
+        description="CarCar is an application designed to streamline the operations of car dealerships.
         It provides managers with an all-in-one solution to efficiently track sales, manage inventory, and oversee service operations.
-        With this user-friendly application, you can effortlessly add and manage salespeople, customers, and service employees, ensuring smooth and effective dealership management."
+        With this convenient application, you can effortlessly add and manage salespeople, customers, and service employees, ensuring smooth and effective dealership management."
         link="https://gitlab.com/CBeckAskenaizer/carcar"
         img={{ src: '/assets/images/3d-rendering-illustration-of-a-lowpoly-car-free-png.png', alt: 'Project Fire' }}
         category={
